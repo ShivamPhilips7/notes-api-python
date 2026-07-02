0593230b35927b53e6a501cea1f112a8806f249f
ShivamPhilips7 <Shivam.RajkumarSingh@philips.com>
2026-07-02 23:44:27 +0530

    Yeah

--- deploy.ps1
+++ deploy.ps1
@@ -3,3 +3,14 @@ kubectl apply -f k8s/
 kubectl rollout restart deployment notes-api
 
 kubectl rollout status deployment notes-api
+
+Write-Host ""
+Write-Host "Pods:"
+kubectl get pods
+
+Write-Host ""
+Write-Host "Services:"
+kubectl get svc
+
+Write-Host ""
+Write-Host "Deployment completed successfully!"
