24cabe7d0f7eb6211a92c7b5c80520dc7987ebad
ShivamPhilips7 <Shivam.RajkumarSingh@philips.com>
2026-07-03 12:06:08 +0530

    Woah

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,18 @@
-kubectl apply -f k8s/
+$namespace = "notes-api"
 
-kubectl rollout restart deployment notes-api
+kubectl apply -f k8s/ -n $namespace
 
-kubectl rollout status deployment notes-api
+kubectl rollout restart deployment/notes-api -n $namespace
+
+kubectl rollout status deployment/notes-api -n $namespace
 
 Write-Host ""
 Write-Host "Pods:"
-kubectl get pods
+kubectl get pods -n $namespace
 
 Write-Host ""
 Write-Host "Services:"
-kubectl get svc
+kubectl get svc -n $namespace
 
 Write-Host ""
 Write-Host "Deployment completed successfully!"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
 echo "======================================"
+echo "Waiting for PostgreSQL..."
+echo "======================================"
+
+python - <<EOF
+import socket
+import time
+
+host = "postgres-service"
+port = 5432
+
+while True:
+    try:
+        with socket.create_connection((host, port), timeout=2):
+            print("PostgreSQL is ready!")
+            break
+    except OSError:
+        print("Waiting for PostgreSQL...")
+        time.sleep(2)
+EOF
+
+echo "======================================"
 echo "Running Alembic migrations..."
 echo "======================================"
 
